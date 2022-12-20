33fe1ab946d1a91e6a3eb4ce44592f451ae0a0ff
tuba-gibi <eetede84@gmail.com>
2022-12-20 20:51:07 +0300

    bsm

--- assignment2/basamak/app.js
+++ assignment2/basamak/app.js
@@ -1,1 +1,7 @@
-let num 
+let num = +prompt("Basamak sayisini ogrenmek istediginiz bir sayi giriniz.");
+let bsm = 0;
+while (num != 0) {
+  num = Math.floor(num / 10);
+  bsm++;
+}
+alert(`Basamak sayisi , ${bsm}`);
