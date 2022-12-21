7d9b115f4809eeb1782b388facd68e6fbd3cae28
tuba-gibi <eetede84@gmail.com>
2022-12-21 09:10:59 +0300

    odd-even

---
+++ assignment2/tek-cift/app.js
@@ -0,0 +1,8 @@
+let num = Number(prompt("Enter a number to check if it is odd or even"));
+if (num % 2 === 0) {
+  alert("even");
+}else if (num % 2 !== 0) {
+  alert("odd");
+}else{
+    console.log("Enter a number to check if it is odd or even");
+}
