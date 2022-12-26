cc790f2015865e6f8ae4defaee07b768a6d399ca
tuba-gibi <eetede84@gmail.com>
2022-12-26 13:38:09 +0300

    biggest again

---
+++ team_dec_26/team.js
@@ -0,0 +1,11 @@
+const n1 = +prompt("Enter a number")
+const n2 = +prompt("Enter a number")
+const n3 = +prompt("Enter a number")
+
+let biggest = n1;
+if (n2 >= biggest) {
+    biggest = n2
+}if (n3>=biggest) {
+    biggest = n3
+}
+console.log( `${biggest} is the biggest.`);
