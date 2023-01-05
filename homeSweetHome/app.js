6f93b4fef81f618c054e5585ade61e1de19f823f
tuba-gibi <eetede84@gmail.com>
2023-01-05 12:13:23 +0300

    switch

--- homeSweetHome/app.js
+++ homeSweetHome/app.js
@@ -1,10 +1,56 @@
-let numberr = +prompt(`Please enter a number`);
-if (numberr > 0) {
-  alert(`${numberr} is positive`);
-}else if (numberr < 0) {
-  alert(`${numberr} is negative`);
-}else if (numberr === 0) {
-  alert(`It is zero`);
-}else{
-  alert(`It's not a number.Please enter a number`);
-}alert
+// let numberr = +prompt(`Please enter a number`);
+// if (numberr > 0) {
+//   alert(`${numberr} is positive`);
+// } else if (numberr < 0) {
+//   alert(`${numberr} is negative`);
+// } else if (numberr === 0) {
+//   alert(`It is zero`);
+// } else {
+//   alert(`It's not a number.Please enter a number`);
+// }
+
+//***** BIGGEST NUMBER **** */
+
+// let num1 = +prompt(`Please enter a number`);
+// let num2 = +prompt(`Please enter another number`);
+// let num3 = +prompt(`Please enter another number`);
+// let biggest = num1;
+// if (num2 > biggest) {
+//   console.log(`${num2} is the biggest`);
+// } else if (num3 > biggest) {
+//   console.log(`${num3} is the biggest`);
+// } else if (num1 > biggest) {
+//   console.log(`${num1} is the biggest`);
+// }else{
+//     console.log("We have a problem :) Please try again.");
+// }
+
+//**SWITCH-CASE */
+
+let n1 = +prompt("Please enter a number")
+let opr = prompt("Please choose the operation +/-/ * / /")
+let n2 = +prompt("Please enter a number")
+let result
+switch (opr) {
+  case `+`:
+    result = n1 + n2
+    break;
+  case `-`:
+    result = n1 - n2
+
+    break;
+  case `*`:
+    result = n1 * n2
+
+    break;
+  case `/`:
+    result = n1 / n2
+
+    break;
+
+  default:
+    // alert("Wrong entrance..")
+    break;
+}
+// console.log(`Result is ${result}`)
+console.log(`Result is  ${result}`);
